ea935bed609ea73679a70529fb91012fefcb3d0b
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2024-12-24 08:44:18 +0700

    navbar, hero section

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: 'Poppins'
+      },
+      colors: {
+        utama: '#000000',
+        kedua: '#120f0e',
+        ketiga: '#1b1715',
+      }
+    },
+  },
+  plugins: [],
+}
